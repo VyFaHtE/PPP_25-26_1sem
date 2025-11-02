640a28e46d1c4a366a4f311eaf99aa05e6856223
VyFaHtE <Chrshes@yandex.ru>
2025-11-02 23:19:47 +0300

    Update main.py

--- 3lab/main.py
+++ 3lab/main.py
@@ -1,4 +1,19 @@
+#Лабораторная работа номер 3, вариант 2
 
+def turn(i, sys):
+    answ = ''
+    while i != 0:
+        answ += str(i % sys)
+        i //= sys
+    answ = list(map(int, answ[::-1]))
+    return [0] * (n - len(answ)) + answ
 
-if __name__ == "__main__":
-    pass # Ваш код здесь
+def func(alp, n, i = 0):
+    if i == len(alp) ** n:
+        return
+    print([alp[j] for j in turn(i, len(alp))])
+    func(alp, n, i + 1)
+
+alp = input().split()
+n = int(input())
+func(alp, n)
